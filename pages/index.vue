ffff2bfb125b4182c5922d3fac637defb97e1cb3
stalin solarte <crhis21377@gmail.com>
2019-03-21 20:29:44 -0500

    Iniciando proyecto

---
+++ pages/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>hola {{msg}}</h1>
+    <input type="text" v-model="msg">
+    <button @click="guardar">Guardar</button>
+    <table>
+      <tr>
+        <th>nombre</th>
+      </tr>
+      <tr v-for="(item, index) in municipios" :key="index">
+        <td>{{item}}</td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: "stalin solarte",
+      municipios: ["mocoa", "villagarzon", "pto asis"]
+   
+   };
+  },
+  methods: {
+      guardar(){
+          this.municipios.push(this.msg)
+          this.msg=''
+      }
+  }
+};
+</script>
